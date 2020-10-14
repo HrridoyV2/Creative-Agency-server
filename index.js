f88fc5c733a4c174e4f27dd24dca7ffa9fb04776
unknown <hrridoyv2@gmail.com>
2020-10-14 13:45:53 +0600

    init for deploy

--- index.js
+++ index.js
@@ -3,21 +3,20 @@ const cors = require('cors')
 const bodyParser = require('body-parser');
 require('dotenv').config()
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.newsi.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
 const app = express()
-const = b3nhOkTZHf7HqFoC
 
 app.use(bodyParser.json());
 app.use(cors());
 const port = 5000
 //
     
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://Hrridoy:<password>@cluster0.newsi.mongodb.net/<dbname>?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
+
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+  const collection = client.db(`${process.env.DB_NAME}`).collection("Orders");
+  console.log("DB connected");
 });
 
 //
